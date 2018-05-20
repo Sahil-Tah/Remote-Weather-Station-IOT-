e43285e185c99f384b0f97a53ede3b93d0b62d8f
Sahil Tah <sahil.tah12@gmail.com>
2018-05-21 00:03:37 +0530

    Create csv_reader.py
    Side program for reading CSV file.

---
+++ csv_reader.py
@@ -0,0 +1,19 @@
+import csv
+import datetime
+
+currentDT = datetime.datetime.now()
+date = (currentDT.strftime("%d%b%Y"))
+
+print("\nReports are named as per date, which report would you like to see?")
+print("\nFile name format is as follows: "+ date)
+print("\nEnter the date (DDMonYYY): ")
+file = raw_input()
+file=file+".csv"
+
+
+with open (file,'r') as csv_file:
+        #fieldname= ['Temp C', 'Humidity %','Time']
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+                print(line)
